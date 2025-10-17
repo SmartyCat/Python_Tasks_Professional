6840f49ac6db329fcc4e48437ede7ccfe0347d2d
Kostya <beztalannyjkonstantin@gmail.com>
2025-10-17 07:33:50 +0000

    new version

--- ex/only_numbers.py
+++ ex/only_numbers.py
@@ -4,7 +4,8 @@ total_numbers = 0
 elements = 0
 for i in sys.stdin.readlines():
     try:
-        total_numbers += float(i) if isinstance(i, float) else int(i)
+        i = float(i) if "." in i else int(i)
+        total_numbers += i
     except ValueError:
         elements += 1
 
