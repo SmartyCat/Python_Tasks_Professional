d804024077d8a6467d15e95c0bbe0d6f48972b7e
Kostya <beztalannyjkonstantin@gmail.com>
2025-10-21 15:43:04 +0000

    very easy task with dict

---
+++ functions/date_formatter.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+
+def date_formatter(country_code):
+    d = {
+        "ru": "%d.%m.%Y",
+        "us": "%m-%d-%Y",
+        "ca": "%Y-%m-%d",
+        "br": "%d/%m/%Y",
+        "fr": "%d.%m.%Y",
+        "pt": "%d-%m-%Y",
+    }
+
+    def func(x):
+        return date.strftime(x, d[country_code])
+
+    return func
+
+
+date_ru = date_formatter("ru")
+today = date(2022, 1, 25)
+print(date_ru(today))
+date_ru = date_formatter("us")
+today = date(2025, 1, 5)
+print(date_ru(today))
+date_ru = date_formatter("ca")
+today = date(2015, 12, 7)
+print(date_ru(today))
