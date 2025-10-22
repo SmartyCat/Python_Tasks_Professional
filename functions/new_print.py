5cbc98b1f5875044a9705177eddbeac32f561dbc
Kostya <beztalannyjkonstantin@gmail.com>
2025-10-22 13:15:21 +0000

    not hard task

--- functions/new_print.py
+++ functions/new_print.py
@@ -1,14 +1,12 @@
-def new_print(*args, sep=" ", end="\n"):
-    old_print(
-        *[a.upper() if isinstance(a, str) else a for a in args],
-        sep=sep.upper(),
-        end=end.upper()
-    )
+def dec(func):
+    def wrapper(*args, sep=" ", end="\n"):
+        func(*[str(a).upper() for a in args], sep=sep.upper(), end=end.upper())
+
+    return wrapper
 
 
-old_print = print
-print = new_print
-print("beegeek", [1, 2, 3], 4)
-#print("bee", "geek", sep=" and ", end=" wow")
-words = ("black", "white", "grey", "black-1", "white-1", "python")
-print(*words, sep=" to ", end=" LOVE")
+print = dec(print)
+
+print("hi", "there", end="!")
+print("are you in trouble?")
+print(111, 222, 333, sep="xxx")
