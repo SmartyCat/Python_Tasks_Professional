93a42da9fb9e4d9006ed3c88cf2f55053822a177
Kostya <beztalannyjkonstantin@gmail.com>
2025-10-22 13:31:16 +0000

    very easy task

---
+++ functions/do_twice.py
@@ -0,0 +1,22 @@
+def do_twice(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+@do_twice
+def beegeek():
+    print("beegeek")
+
+
+beegeek()
+
+
+@do_twice
+def beegeek():
+    print("beegeek")
+
+
+print(beegeek())
