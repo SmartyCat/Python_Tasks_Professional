b43a86a5513445112b6aceafb006de4a8a2d3d55
Kostya <beztalannyjkonstantin@gmail.com>
2025-10-24 14:33:51 +0000

    another easy task with iter

---
+++ iter_and_gen/transpose.py
@@ -0,0 +1,11 @@
+def transpose(matrix):
+    return [[*i] for i in zip(*matrix)]
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for row in transpose(matrix):
+    print(row)
+
+matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+for row in transpose(matrix):
+    print(row)
